8108e75cec4e3380546cd6c2ce1f9c2d2861dfc6
neehhaa <nehawasnik95@outlook.com>
2022-06-26 23:18:52 +0530

    Data types length

---
+++ VariablesDemo.java
@@ -0,0 +1,24 @@
+public class VariablesDemo{
+
+	public static void main(String[] args){
+
+		byte b = 100; 		//range[8bits = 1 byte]
+		short s = 199; 		//range[16bits = 2 bytes]
+		char ch = 'a'; 		//range[16bits = 2 bytes]
+		int i = 156999; 		//range[32bits = 4 bytes]
+		long lg = 15L; 		//range[64bits = 8 bytes]
+		float flt = 15.5f; 	//range[32bits = 4 bytes]
+		double doub = 15.25; 	//range[64bits = 8 bytes]
+
+
+		System.out.println(b);
+		System.out.println(s);
+		System.out.println(ch);
+		System.out.println(i);
+		System.out.println(lg);
+		System.out.println(flt);
+		System.out.println(doub);
+
+
+	}
+}
